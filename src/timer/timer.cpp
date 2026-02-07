3adb3452514951a46fbcf9a3f988880fe27f1e2d
Danylo Kalinchuk <kalinchukdanylo@gmail.com>
2026-02-07 10:20:37 +0100

    Timer::running changed to std::atomic

--- src/timer/timer.cpp
+++ src/timer/timer.cpp
@@ -1,7 +1,7 @@
 #include "timer.hpp"
 
 void Timer::handle_function(){
-	while (this->running){
+	while (this->running.load()){
 		std::this_thread::sleep_for(std::chrono::seconds(this->delay_sec));
 		this->function();
 	}
@@ -11,15 +11,16 @@ Timer::Timer(unsigned int delay_sec, void (*function)()){
 	this->function = function;
 	this->delay_sec = delay_sec;
 
-	this->running = true;
+	this->running.store(true);
 	this->timer_thread = std::thread(&Timer::handle_function, this);
 }
 
 Timer::~Timer(){
-	this->running = false;
+	this->running.store(false);
 
 	if (this->timer_thread.joinable()){
 		this->timer_thread.join();
 	}
 }
+
 
